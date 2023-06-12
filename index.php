a7c087fcbb6ec4e7692bbde8660d8eae3e374daf
Kekon130 <72703746+Kekon130@users.noreply.github.com>
2023-06-12 12:50:05 +0000

    Conseguida hacer una petición a ala api de Nominatim para recuperar las coordenadas de una ubicación

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
-  echo "<h1>Hola Mundo!!!!!</h1>";
+$localizacion = "Azuqueca de Henares";
+$url = "https://nominatim.openstreetmap.org/search?q=Azuqueca%20de%20Henares&format=json&email=sergio.plaza@alumnos.upm.es";
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$res = curl_exec($curl);
+
+if ($res === false) {
+  echo "Hubo un error en la petición: " . curl_error($curl);
+} else {
+  echo $res;
+}
+
+curl_close($curl);
 ?>
