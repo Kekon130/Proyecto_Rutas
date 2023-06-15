825388356d9e3a312ebbcad97b38571dc31299be
Kekon130 <72703746+Kekon130@users.noreply.github.com>
2023-06-15 08:54:40 +0000

    Ya se realiza la petición

--- index.php
+++ index.php
@@ -4,10 +4,7 @@ require("./lib_functions.php");
 $method = $_SERVER["REQUEST_METHOD"];
 $path = $_SERVER["REQUEST_URI"];
 
-echo $method;
-echo $path;
-
-if ($method === "GET" && $path === "/distanciaLineaRecta") {
+if ($method === "GET" && strpos($path, "/distanciaLineaRecta") === 0) {
   try {
     echo json_encode(getDistancia2PuntosLineaRecta($_GET["localizacion"]));
   } catch (Exception $ex) {
