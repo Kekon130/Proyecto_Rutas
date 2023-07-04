b2a4f31a435530ba1ddac55ceef2daaddf69e905
Kekon130 <72703746+Kekon130@users.noreply.github.com>
2023-07-04 13:52:23 +0200

    Cambio en la api de trnasporte público y añadidos 3 métodos nuevos

--- lib_functions.php
+++ lib_functions.php
@@ -16,7 +16,7 @@ function getCoordenadas($ubicacion)
     $resultado = array("error" => curl_error($curl));
   } else {
     $aux = json_decode($res, true);
-    $resultado = array("lat" => $aux[0]["lat"], "lon" => $aux[0]["lon"]);
+    $resultado = array("lat" => $aux[0]["lat"], "lng" => $aux[0]["lon"]);
   }
   curl_close($curl);
   return $resultado;
@@ -42,9 +42,9 @@ function calcularModuloDistancia($coordenadas1, $coordenadas2)
 
   // Convertir las coordenadas de grados a radianes
   $latitud1 = deg2rad($coordenadas1["lat"]);
-  $longitud1 = deg2rad($coordenadas1["lon"]);
+  $longitud1 = deg2rad($coordenadas1["lng"]);
   $latitud2 = deg2rad($coordenadas2["lat"]);
-  $longitud2 = deg2rad($coordenadas2["lon"]);
+  $longitud2 = deg2rad($coordenadas2["lng"]);
 
   // Diferencia de latitudes y longitudes
   $deltaLatitud = $latitud2 - $latitud1;
@@ -56,7 +56,7 @@ function calcularModuloDistancia($coordenadas1, $coordenadas2)
   $c = 2 * atan2(sqrt($a), sqrt(1 - $a));
   $distancia = $radioTierra * $c;
 
-  return array("Distancia" => $distancia);
+  return array("distancia" => $distancia);
 }
 
 function getRutaMasRapida2puntosCoche($latitud1, $longitud1, $latitud2, $longitud2)
@@ -86,9 +86,9 @@ function getRutaMasRapidaCoche($origen)
 {
   $coordenadas = getParDeCoordenadas($origen);
   $latitud1 = $coordenadas["origen"]["lat"];
-  $longitud1 = $coordenadas["origen"]["lon"];
+  $longitud1 = $coordenadas["origen"]["lng"];
   $latitud2 = $coordenadas["destino"]["lat"];
-  $longitud2 = $coordenadas["destino"]["lon"];
+  $longitud2 = $coordenadas["destino"]["lng"];
   return json_encode(getRutaMasRapida2puntosCoche($latitud1, $longitud1, $latitud2, $longitud2));
 }
 
@@ -96,20 +96,20 @@ function getRutaMasRapidaTransportePublico($origen)
 {
   $coordenadas = getParDeCoordenadas($origen);
   $latitud1 = $coordenadas["origen"]["lat"];
-  $longitud1 = $coordenadas["origen"]["lon"];
+  $longitud1 = $coordenadas["origen"]["lng"];
   $latitud2 = $coordenadas["destino"]["lat"];
-  $longitud2 = $coordenadas["destino"]["lon"];
+  $longitud2 = $coordenadas["destino"]["lng"];
   return json_encode(getRutaMasRapida2puntosTP($latitud1, $longitud1, $latitud2, $longitud2));
 }
 
 function getRutaMasRapida2puntosTP($latitud1, $longitud1, $latitud2, $longitud2)
 {
   $resultado = null;
-  $api_key = parse_ini_file("./config.ini")["mapquest_key"];
+  $api_key = parse_ini_file("./config.ini")["here_public_transport_key"];
 
-  $url = "https://www.mapquestapi.com/directions/v2/route?key=" . urlencode($api_key) .
-    "&from=" . urlencode($latitud1) . "," . urlencode($longitud1) . "&to=" . urlencode($latitud2) .
-    "," . urlencode($longitud2) . "&outFormat=json&unit=k";
+  $url = "https://transit.router.hereapi.com/v8/routes?origin=" . urlencode($latitud1) . "," .
+    urlencode($longitud1) . "&destination=" . urlencode($latitud2) . "," . urlencode($longitud2) .
+    "&apikey=" . urlencode($api_key);
   $curl = curl_init($url);
   curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
@@ -117,20 +117,52 @@ function getRutaMasRapida2puntosTP($latitud1, $longitud1, $latitud2, $longitud2)
   if ($res === false) {
     $resultado = array("error" => curl_error($curl));
   } else {
-    $aux = json_decode($res, true)["route"];
-    $resultado = array("distancia" => $aux["distance"], "tiempo" => $aux["time"], "formatted time" => $aux["formattedTime"]);
+    $aux = json_decode($res, true)["routes"][0]["sections"];
+    $resultado = array("distancia" => getDistanceOfTrip($aux), "tiempo" => getTimeOfTrip($aux));
   }
   curl_close($curl);
   return $resultado;
+}
+
+function getDistanceOfTrip($array)
+{
+  $distance = 0;
+  for ($i = 0; $i < count($array); $i++) {
+    $section = $array[$i];
+    $coordenadas1 = $section["departure"]["place"]["location"];
+    $coordenadas2 = $section["arrival"]["place"]["location"];
+    $distance += calcularModuloDistancia($coordenadas1, $coordenadas2)["distancia"];
+  }
+  return $distance;
+}
+
+function getTimeOfTrip($array)
+{
+  $time = 0;
+  for ($i = 0; $i < count($array); $i++) {
+    $section = $array[$i];
+    $date1 = $section["departure"]["time"];
+    $date2 = $section["arrival"]["time"];
+    $time += getDiffDates($date1, $date2);
+  }
+  return array("horas" => ($time / 3600), "minutos" => (($time % 3600) / 60), "segundos" => $time % 60);
+}
+
+function getDiffDates($inicDate, $endDate)
+{
+  $date1 = new DateTime($inicDate);
+  $date2 = new DateTime($endDate);
+  $diff = $date1->diff($date2);
+  return $diff->s + ($diff->i * 60) + ($diff->h * 3600);
 }
 
 function getInformation($origen)
 {
   $coordenadas = getParDeCoordenadas($origen);
   $latitud1 = $coordenadas["origen"]["lat"];
-  $longitud1 = $coordenadas["origen"]["lon"];
+  $longitud1 = $coordenadas["origen"]["lng"];
   $latitud2 = $coordenadas["destino"]["lat"];
-  $longitud2 = $coordenadas["destino"]["lon"];
+  $longitud2 = $coordenadas["destino"]["lng"];
 
   $distanciaLineaRecta = calcularModuloDistancia($coordenadas["origen"], $coordenadas["destino"]);
   $distanciaCoche = getRutaMasRapida2puntosCoche($latitud1, $longitud1, $latitud2, $longitud2);
